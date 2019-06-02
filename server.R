371dbc6b64ecc4878933fb0738808090d14cbd7b
Andrew Moskowitz <andrewlmoskowitz@gmail.com>
2019-06-02 16:31:24 -0700

    Wire frame app
    These files create a wireframe app that works for individual scenarios. 
    Need to make a bunch of improvements but this is a good first step.

--- server.R
+++ server.R
@@ -1,26 +1,52 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+server <- function(input, output) {
   
-})
+  output$ui <- renderUI({
+    if (is.null(input$dollar.Perc))
+      return()
+    
+    # Depending on input$input_type, we'll generate a different
+    # UI component and send it to the client.
+    switch(input$dollar.Perc,
+           "dollar" = sliderInput("down.payment",
+                                  'Down Payment:',
+                                  min = 10000,
+                                  max = 500000,
+                                  step = 1000,
+                                  value = 50000),
+           "perc" =  sliderInput("dperc",
+                                 "Percentage of Downpayment:",
+                                 min = 1,
+                                 max = 50,
+                                 step = .05,
+                                 value = 20))})
+  
+  
+  output$graphplaceholder <- renderPlot(plot(input$home.price))
+  numbers = reactive({input$home.price - input$after.tax.income})
+  # home.price
+  # after.tax.income
+  # years
+  # interest.rate
+  total_months <- reactive({12*input$years})
+  mon_rate <- reactive({(input$interest.rate)/100/12})
+  discount_rate <- reactive({1-((1+mon_rate())^-total_months())})
+  dp <- reactive({input$home.price*.20})
+  purchase.price <- reactive({input$home.price - dp()})
+  final<- reactive({(purchase.price()*mon_rate()*total_months())/discount_rate()})
+  monthly_cost <- reactive({final()/total_months()})
+  remaining.income <- reactive({input$after.tax.income - monthly_cost()})
+
+  
+  output$scenario.table <- renderTable((t(data.frame("price" = input$home.price,
+                                                   "Loan Amount" = purchase.price(),
+                                                   "Annual Rate" = input$interest.rate,
+                                                   "Monthly After Tax Income" = input$after.tax.income,
+                                                   "Monthly Cost" = monthly_cost(),
+                                                   "Remaining Income" = remaining.income(),
+                                                   "Total Cost of House" = final()))),rownames= TRUE)
+  
+}
+
+shinyApp(ui, server)
--- ui.R
+++ ui.R
@@ -1,16 +1,10 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(DT)
+library(data.table)
 
 # Define UI for application that draws a histogram
-ui <- shinyUI(fluidPage(
+ui <- shinyUI(
+  fluidPage(
   
   # Application title
   titlePanel("Mortgage Calculator"),
@@ -18,54 +12,48 @@ ui <- shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput(inputId = "homeprice",
-                   label ="Home Price:",
-                   min = 100000,
-                   max = 3000000,
-                   step = 10000,
-                   value = 500000),
-       sliderInput("aftertaxincome",
-                   "After Tax Income:",
-                   min = 1000,
-                   max = 50000,
-                   step = 100,
-                   value = 4000),
-       sliderInput("years",
-                   "Duration of Loan:",
-                   min = 10,
-                   max = 30,
-                   step = 5,
-                   value = 30),
-       sliderInput("rate",
-                   "Annual Rate:",
-                   min = 1.0,
-                   max = 10.0,
-                   step = .01,
-                   value = 4.25),
-       selectInput("dollarPerc", "Down Payment Option",
-                   c(Dollar = 'dollar',
-                     Percent = 'perc')),
-       conditionalPanel(
-         condition = "input.dollarPerc == 'dollar'",
-         sliderInput("downpayment",
-                     'Down Payment:',
-                     min = 10000,
-                     max = 500000,
-                     step = 1000,
-                     value = 50000)),
-         conditionalPanel(
-         condition = "input.dollarPerc == 'perc'",
-         sliderInput("dperc",
-                     "Percentage of Downpayment:",
-                     min = 1,
-                     max = 50,
-                     step = .05,
-                     value = 20))),
-       
-       mainPanel(
-         plotOutput(outputId = 'distPlot')
-       )
-       )       ))
+      sliderInput(inputId = "home.price",
+                  label ="Home Price:",
+                  min = 100000,
+                  max = 3000000,
+                  step = 10000,
+                  value = 500000),
+      sliderInput(inputId = "after.tax.income",
+                  label = "Monthly After Tax Income:",
+                  min = 1000,
+                  max = 25000,
+                  step = 50,
+                  value = 4000),
+      sliderInput(inputId = "years",
+                  label = "Duration of Loan:",
+                  min = 10,
+                  max = 30,
+                  step = 5,
+                  value = 30),
+      sliderInput(inputId = "interest.rate",
+                  label = "Annual Rate:",
+                  min = 1.0,
+                  max = 10.0,
+                  step = .01,
+                  value = 4.25),
+      selectInput(inputId = "dollar.Perc", 
+                  label = "Down Payment Option",
+                  c("Dollar" = 'dollar',
+                    "Percent" = 'perc'),
+                  selected = 'dollar'),
+      
+      uiOutput("ui")),
+      
+    mainPanel(
+      
+      #plotOutput("graphplaceholder"),
+      br(), br(),
+      
+      tableOutput("scenario.table")
+      
+      
+    )
+  )))
 
 
 
